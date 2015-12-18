2d1428c1dc744c67c9570caff8aacd4f486692fd
nadim <nadimndc@gmail.com>
2015-12-19 03:16:41 +0600

    tried to connect php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <title>this is my php</title>
+</head>
+<body>
+  <form>
+<h1>This is the login page</h1>
+<pre>
+Username:<input type="text" name="uname" />
+password:<input type="password" name="uname">
+         <input type="submit" value="submit" name="sub"/>
+         <button type="button" name="reg">Register</button>
+</pre>
+</body>
